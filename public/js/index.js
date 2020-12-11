74fb323c6b210cca11ba958f1ea9206b14279fb1
Bui Gábor <bui.gabor97@gmail.com>
2020-12-11 11:23:33 +0100

    Re-added lessonHelper function

--- public/js/index.js
+++ public/js/index.js
@@ -122,6 +122,46 @@ function lessonHandler(e) {
 	}
 }
 
+function lessonHelper({
+	varName,
+	eventListener,
+	classList,
+	attribute,
+	textContent,
+	innerHTML,
+	id,
+}) {
+	if (eventListener) {
+		varName.addEventListener(
+			Object.keys(eventListener)[0],
+			Object.values(eventListener)[0],
+		);
+	}
+	if (classList) {
+		for (let i = 0; i < classList.length; i++) {
+			varName.classList.add(classList[i]);
+		}
+	}
+	if (attribute) {
+		for (let i = 0; i < attribute.length; i++) {
+			varName.setAttribute(
+				Object.keys(attribute[i])[0],
+				Object.values(attribute[i])[0],
+			);
+		}
+	}
+	if (textContent) {
+		varName.textContent = textContent;
+	}
+	if (innerHTML) {
+		varName.innerHTML = innerHTML;
+	}
+	if (id) {
+		varName.id = id;
+	}
+	return varName;
+}
+
 function handleViewClick(lesson) {
 	const title = lesson.querySelector('.lesson-card-title').innerText;
 	const content = lesson.querySelector('.lesson-card-content').innerHTML;
