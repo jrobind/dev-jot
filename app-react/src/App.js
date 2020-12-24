29b0b8eb2cf61f9b4c9a160c499badf381965027
lucievr <lucy.vrsovska@gmail.com>
2020-12-24 13:08:34 +0100

    Create components and images folder and import avatars svgs

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,24 +1,38 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <img src={logo} className="App-logo" alt="logo" style={{height: '100px'}} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    this.state = {
+
+    }
+  }
+
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={logo} className="App-logo" alt="logo" style={{height: '100px'}} />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
