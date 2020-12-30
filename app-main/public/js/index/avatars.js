d16075990e5f715b8df2d362ab478af366b7d4b4
unknown <jmiccolo89@gmail.com>
2020-12-29 20:51:11 -0500

    Fixing code for modulization

--- app-main/public/js/index/avatars.js
+++ app-main/public/js/index/avatars.js
@@ -1,4 +1,4 @@
-export const avatars = [
+const avatars = [
   "bear",
   "butterfly",
   "elephant",
@@ -8,3 +8,5 @@ export const avatars = [
   "octopus",
   "parrot",
 ];
+
+export default avatars;
