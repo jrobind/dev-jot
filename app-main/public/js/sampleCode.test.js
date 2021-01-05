7c1b1a5381ee4fcf3ca54a32debff2795755b676
unknown <jmiccolo89@gmail.com>
2021-01-05 18:27:27 -0500

    fixing rebase

--- app-main/public/js/sampleCode.test.js
+++ app-main/public/js/sampleCode.test.js
@@ -1,5 +1,5 @@
-const sampleCode = require('./sampleCode');
+const sampleCode = require("./sampleCode");
 
-test('This verifies that sampleCode sends a bool response', () => {
-    expect(sampleCode()).toEqual(false);
-})
+test("This verifies that sampleCode sends a bool response", () => {
+  expect(sampleCode()).toEqual(false);
+});
